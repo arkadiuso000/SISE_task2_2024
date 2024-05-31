d63b8400acc49a9cd3a294213534195a583087ca
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-05-31 22:49:18 +0200

    initial packages

---
+++ my_functions.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam, SGD
+import matplotlib.pyplot as plt
+
